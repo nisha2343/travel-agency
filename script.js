40e6cd113fae199b26739a3d73f380d865ef8a40
Nisha <119412905+nisha2343@users.noreply.github.com>
2024-09-26 17:44:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+/*const box = document.getElementsByClassName('lider')box1.addEventListener('click',()=>{
+    console.log('click by event');
+})*/
+
+let box = document.querySelectorAll('button');
+box.addEventListener('click', )
